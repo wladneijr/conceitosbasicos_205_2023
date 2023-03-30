95c3c5aaca9135d885884300fd22b500d99beb5e
Aula <Aula@CANP-LABTI7-C24>
2023-03-30 15:40:35 -0300

    Adicionar arquivos de projeto.

---
+++ conceitosbasicos/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+
+Console.WriteLine("Minha altura em nano é " +altura+ " e meu peso é "+ peso +" e em nano é "+ nanoAltura + "nm");
+
+int idad = 30;
+
+Console.WriteLine("A idade de Marcos é "+ idad +"!");
+
+double ida = 30.0;
+
+Console.WriteLine("A idade de Marcos é "+ (int)ida +"!");
+
+string parcela1 = "10";
+string parcela2 = "20";
+
+Console.WriteLine(parcela1+parcela2);
